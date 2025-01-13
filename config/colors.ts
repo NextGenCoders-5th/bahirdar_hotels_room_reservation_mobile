1799f37e99d9712217c14b0dd887095ec4d90ac5
tewodros-m <tewodrosmengie@gmail.com>
2025-01-13 19:43:19 +0300

    define colors

---
+++ config/colors.ts
@@ -0,0 +1,17 @@
+export default {
+  primaryDark: 'rgba(107, 84, 251, 0.95)',
+  primary: '#A294F9',
+  primaryMedium: '#CDC1FF',
+  primaryLight: '#E5D9F2',
+  primaryExtraLight: '#F5EFFF',
+
+  darkGrey: 'rgba(67, 67, 70, 0.9)',
+  grey: 'rgba(146, 146, 149, 0.9)',
+  lightGrey: 'rgba(226, 226, 231, 0.9)',
+
+  black: '#000',
+  white: '#fff',
+
+  red: '#FF453A',
+  green: '#34C759',
+};
