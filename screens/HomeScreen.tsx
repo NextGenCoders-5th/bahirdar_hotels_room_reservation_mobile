bbb6e04bf6d1508eaa6a78326e29fd17504f12de
tewodros-m <tewodrosmengie@gmail.com>
2025-01-15 21:35:14 +0300

    feat: add search field

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import HotelsList from '../components/HotelsList';
 import colors from '@/config/colors';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Search hotels' style={styles.input}></TextInput>
+        <Ionicons
+          style={styles.icon}
+          name='search'
+          size={30}
+          color={colors.grey}
+        />
+      </View>
       <Text style={styles.title}>Featured Hotels</Text>
       <HotelsList />
     </View>
@@ -16,11 +26,46 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    backgroundColor: colors.white,
+    width: '100%',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
     color: colors.black,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 50,
+    backgroundColor: colors.greyLight,
+    borderColor: colors.primary,
+    borderWidth: 1,
+    borderRadius: 15,
+    marginVertical: 10,
+    marginBottom: 20,
+  },
+  icon: {
+    width: 60,
+    height: '100%',
+    textAlign: 'center',
+    backgroundColor: colors.primary,
+    padding: 10,
+    color: colors.white,
+    borderRadius: 15,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+  },
+  input: {
+    flex: 1,
+    height: '100%',
+    padding: 15,
+    borderRadius: 15,
+    borderRightWidth: 0,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    fontSize: 16,
+    color: colors.greyDark,
   },
 });
