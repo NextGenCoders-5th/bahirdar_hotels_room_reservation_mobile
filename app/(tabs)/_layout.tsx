c930ce6c8dbd373e9579655913a876098a3aa8c7
tewodros-m <tewodrosmengie@gmail.com>
2025-01-16 15:58:53 +0300

    config: configure color theme

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,82 +1,85 @@
 import { Tabs } from 'expo-router';
 import colors from '@/config/colors';
 import TabBarIcon from '@/components/TabBarIcon';
+import { ThemeProvider } from '@react-navigation/native';
+import appTheme from '@/config/appTheme';
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: colors.primaryDark,
-        tabBarInactiveTintColor: colors.grey,
-        tabBarStyle: {
-          borderTopWidth: 0,
-          height: 60,
-          marginVertical: 5,
-        },
-
-        tabBarLabelStyle: {
-          marginTop: 5,
-          fontSize: 12,
-          // fontWeight: 'bold',
-          // color: colors.grey,
-        },
-        tabBarLabelPosition: 'below-icon',
-      }}
-    >
-      <Tabs.Screen
-        name='index'
-        options={{
-          title: 'Home',
-
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon
-              name={focused ? 'home-sharp' : 'home-outline'}
-              focused={focused}
-            />
-          ),
+    <ThemeProvider value={appTheme}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: colors.primaryDark,
+          tabBarInactiveTintColor: colors.grey,
+          tabBarStyle: {
+            borderTopWidth: 0,
+            height: 60,
+          },
+          tabBarHideOnKeyboard: true,
+          tabBarLabelStyle: {
+            marginTop: 5,
+            fontSize: 12,
+            // fontWeight: 'bold',
+            // color: colors.grey,
+          },
+          tabBarLabelPosition: 'below-icon',
         }}
-      />
+      >
+        <Tabs.Screen
+          name='index'
+          options={{
+            title: 'Home',
 
-      <Tabs.Screen
-        name='favorites'
-        options={{
-          title: 'Favorites',
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                name={focused ? 'home-sharp' : 'home-outline'}
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon
-              name={focused ? 'heart-sharp' : 'heart-outline'}
-              focused={focused}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='bookings'
-        options={{
-          title: 'Bookings',
+        <Tabs.Screen
+          name='favorites'
+          options={{
+            title: 'Favorites',
 
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon
-              name={focused ? 'calendar-clear' : 'calendar-clear-outline'}
-              focused={focused}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='profile'
-        options={{
-          title: 'Profile',
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                name={focused ? 'heart-sharp' : 'heart-outline'}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='bookings'
+          options={{
+            title: 'Bookings',
 
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon
-              name={focused ? 'person-circle' : 'person-circle-outline'}
-              focused={focused}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                name={focused ? 'calendar-clear' : 'calendar-clear-outline'}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='profile'
+          options={{
+            title: 'Profile',
+
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                name={focused ? 'person-circle' : 'person-circle-outline'}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </ThemeProvider>
   );
 }
