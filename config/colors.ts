2c3ae3065b015300d676e70a3a19075d8b67128d
tewodros-m <tewodrosmengie@gmail.com>
2025-01-20 00:08:37 +0300

    chore(colors): add color

--- config/colors.ts
+++ config/colors.ts
@@ -1,12 +1,13 @@
 export default {
   primaryDark: 'rgb(106, 84, 251)',
   primary: '#A294F9',
-  primaryMedium: '#CDC1FF',
+  primaryMediumLight: '#CDC1FF',
   primaryLight: '#E5D9F2',
   primaryExtraLight: '#F5EFFF',
   primaryExtraLight2: 'rgb(240, 237, 245)',
 
   greyDark: 'rgba(37, 37, 37, 0.9)',
+  greyMediumDark: 'rgba(63, 63, 63, 0.9)',
   grey: 'rgba(124, 124, 126, 0.9)',
   greyLight: 'rgba(211, 211, 211, 0.9)',
 
