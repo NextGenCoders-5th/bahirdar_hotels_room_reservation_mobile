c7bb708732a7a2fe6ed0e30623ca3eb9a8920252
tewodros-m <tewodrosmengie@gmail.com>
2025-01-20 12:16:59 +0300

    chore(app): add layout theme and remove nested screen in GetStartedScreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
 import { Stack } from 'expo-router';
 import { Provider } from 'react-redux';
 import { store } from '@/redux/store';
+import { ThemeProvider } from '@react-navigation/native';
+import appTheme from '@/config/appTheme';
 
 export default function RootLayout() {
   return (
-    <Provider store={store}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='(tabs)' />
-        <Stack.Screen name='(auth)' />
-      </Stack>
-    </Provider>
+    <ThemeProvider value={appTheme}>
+      <Provider store={store}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name='(tabs)' />
+          <Stack.Screen name='(auth)' />
+        </Stack>
+      </Provider>
+    </ThemeProvider>
   );
 }
--- screens/GetStartedScreen.tsx
+++ screens/GetStartedScreen.tsx
@@ -16,50 +16,48 @@ export default function GetStartedScreen() {
   const router = useRouter();
 
   return (
-    <Screen>
-      <View style={styles.container}>
-        <Text style={styles.title}>Let's get started</Text>
-        <Image
-          style={styles.image}
-          source={require('@/assets/images/get-started.png')}
-        />
-        <View
-          style={{
-            width: '100%',
-            gap: 5,
-          }}
-        >
-          <View style={{ padding: 20 }}>
-            <AppButton
-              title='Sign up'
-              onPress={() => router.push('/signup')}
-              boxStyle={{
-                backgroundColor: colors.yellow,
+    <View style={styles.container}>
+      <Text style={styles.title}>Let's get started</Text>
+      <Image
+        style={styles.image}
+        source={require('@/assets/images/get-started.png')}
+      />
+      <View
+        style={{
+          width: '100%',
+          gap: 5,
+        }}
+      >
+        <View style={{ padding: 20 }}>
+          <AppButton
+            title='Sign up'
+            onPress={() => router.push('/signup')}
+            boxStyle={{
+              backgroundColor: colors.yellow,
+            }}
+            textStyle={{
+              color: colors.greyDark,
+            }}
+          />
+        </View>
+        <View style={{ flexDirection: 'row', alignSelf: 'flex-start' }}>
+          <Text style={{ color: colors.white, fontSize: 14, marginRight: 2 }}>
+            Already have an account?
+          </Text>
+          <Pressable onPress={() => router.push('/signin')}>
+            <Text
+              style={{
+                textDecorationLine: 'underline',
+                color: colors.yellow,
+                fontSize: 14,
               }}
-              textStyle={{
-                color: colors.greyDark,
-              }}
-            />
-          </View>
-          <View style={{ flexDirection: 'row', alignSelf: 'flex-start' }}>
-            <Text style={{ color: colors.white, fontSize: 14, marginRight: 2 }}>
-              Already have an account?
+            >
+              Sign in
             </Text>
-            <Pressable onPress={() => router.push('/signin')}>
-              <Text
-                style={{
-                  textDecorationLine: 'underline',
-                  color: colors.yellow,
-                  fontSize: 14,
-                }}
-              >
-                Sign in
-              </Text>
-            </Pressable>
-          </View>
+          </Pressable>
         </View>
       </View>
-    </Screen>
+    </View>
   );
 }
 
