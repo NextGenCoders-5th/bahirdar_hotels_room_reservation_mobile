17db862a31b616c86329abc2a8d7325feadc3da9
tewodros-m <tewodrosmengie@gmail.com>
2025-02-18 10:29:19 +0300

    redirect logged in user to home

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 import WelcomeScreen from '@/screens/WelcomeScreen';
 import Screen from '@/components/Screen';
+import { useAuthContext } from '@/contexts/AuthContext';
+import HomeScreen from '@/screens/HomeScreen';
 
 export default function Welcome() {
+  const { user } = useAuthContext();
+  if (user) {
+    return (
+      <Screen>
+        <HomeScreen />
+      </Screen>
+    );
+  }
   return (
     <Screen>
       <WelcomeScreen />
