10e3ab33a574cfc5292c94cb68865b25a3274fc5
tewodros-m <tewodrosmengie@gmail.com>
2025-02-18 10:51:38 +0300

    redirect to tabs/home instead of showing home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import WelcomeScreen from '@/screens/WelcomeScreen';
 import Screen from '@/components/Screen';
 import { useAuthContext } from '@/contexts/AuthContext';
-import HomeScreen from '@/screens/HomeScreen';
+import { router } from 'expo-router';
 
 export default function Welcome() {
   const { user } = useAuthContext();
-  if (user) {
-    return (
-      <Screen>
-        <HomeScreen />
-      </Screen>
-    );
-  }
+
+  useEffect(() => {
+    if (user) {
+      router.replace('/(tabs)/home');
+    }
+  }, [user]);
+
   return (
     <Screen>
       <WelcomeScreen />
